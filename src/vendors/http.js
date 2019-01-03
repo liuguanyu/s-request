cdc40cec750d07a17b9375ed6ebc0dd084dd2d98
liuguanyu <lgy573@126.com>
2019-01-03 18:30:57 +0800

    refactory post & get

--- src/vendors/http.js
+++ src/vendors/http.js
@@ -15,6 +15,17 @@ export default class {
             case 'post':
                 return await axios.post(opts.url, params);
                 break;
+            case 'put':
+                return await axios.put(opts.url, params);
+                break;
+            case 'patch':
+                return await axios.patch(opts.url, params);
+                break;
+            case 'delete':
+                return await axios.delete(opts.url, {
+                    data: params
+                });
+                break;
             case 'get':
             default:
                 return await axios.get(opts.url, {
