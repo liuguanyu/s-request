e0dcc72cc50b8e7769706155aed76d99fa95fb27
liuguanyu <lgy573@126.com>
2019-01-16 18:00:15 +0800

    -

--- index.js
+++ index.js
@@ -1,21 +1,3 @@
 import SRquest from './src/s-request';
 
-if (typeof window !== 'undefined' && window.Vue) {
-    const install = (Vue, opts = {}) => {
-        Vue.component(SRquest.name, SRquest);
-
-        let mixin = {
-            created: () => {
-                this.baseOpts = opts ? opts : {};
-            }
-        };
-
-        Vue.mixin(mixin);
-    }
-
-    if (typeof window !== 'undefined' && window.Vue) {
-        install(window.Vue, opts);
-    }
-}
-
 export default SRquest;
